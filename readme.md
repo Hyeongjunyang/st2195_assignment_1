0527a1c8f1df64af46fd721fe05ff2c6b227d26a
Hyeongjun <hyeong0809@gmail.com>
2022-02-22 18:26:48 +0900

    programs to print 1 to 10

---
+++ readme.md
@@ -0,0 +1,4 @@
+This is my readme.md file
+
+This repository contains a set of programs
+that prints integers from 1 to 10
---
+++ spyder/proj_python/integers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 17:43:54 2022
+
+@author: hyeon
+"""
+#%%
+print(1,2,3,4,5,6,7,8,9,10)
+
+#%%
+for i in range(1,11):
+    print(i)
+
+print("That's all")
